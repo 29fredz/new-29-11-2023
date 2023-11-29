e1749a25a77ada57abf35db0290f1abc97c9daf1
fred <zermano.fred@hotmail.fr>
2023-11-29 16:47:11 +0100

    bouton clicked dans les states de la fonction Button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-        Hello World
-        <hr/>
-        <Button text="Salut !" type="button" />
-        <Button text="Holla !" type="submit" />
-        <Button text="Hi !" type="reset" />
-        <Button text="Hello !" size={{margin: {right:"25px", left:"3px"}}} />
-    </div>
-  );
+class App extends React.Component<any,any> {
+  constructor(props:any) {
+    super(props);
+    this.state={
+      value:0
+    };
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+          Valeur : {this.state.value}
+          <hr/>
+          <Button text="Add 1" type="button" onBoutonClick={()=> this.setState({value: this.state.value+1})}/>
+          <Button text="Substract 1"  onBoutonClick={()=> this.setState({value: this.state.value-1})}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Button.module.css'
 import PropTypes from "prop-types"
+
 const Button = (props) => {
     console.log(props);
-    return <button type={props.type} className={styles.Button}>{props.text}</button>
+
+    const [clicked, setclicked] = useState({isClicked:false, info2:"coucou"});
+
+    return <button type={props.type} className={styles.Button+(clicked.isClicked?" "+styles.clicked:"")} onClick={(evt) => {
+        props.onBoutonClick();
+        setclicked({isClicked:true, info2:"coucou"});
+        setTimeout(()=>setclicked({isClicked:false, info2:"coucou"}), 350)
+    }}>{props.text}</button>
 }
+
 
 Button.propTypes={
     text:PropTypes.string.isRequired,
@@ -15,7 +24,8 @@ Button.propTypes={
             left:PropTypes.string,
             top:PropTypes.string
         })
-    })
+    }),
+    onBoutonClick:PropTypes.func
 }
 
 Button.defaultProps={
