5c3b8e86174ad4f0ee1e98afc114bf6a5ff771e8
fred <zermano.fred@hotmail.fr>
2023-11-29 17:17:57 +0100

    useEffect

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Button.module.css'
 import PropTypes from "prop-types"
 
@@ -7,11 +7,24 @@ const Button = (props) => {
 
     const [clicked, setclicked] = useState({isClicked:false, info2:"coucou"});
 
+    useEffect(() => {
+        // ici le componentDidMount
+
+
+        // fonction de componentWillUnmount
+        return () => {
+
+        }
+    }, []);
+
+    useEffect(() => {
+       if(clicked.isClicked) setTimeout(()=>setclicked({...clicked, isClicked:false}), 350)
+    }, [clicked])
+
     return <button type={props.type} className={styles.Button+(clicked.isClicked?" "+styles.clicked:"")} onClick={(evt) => {
         props.onBoutonClick();
         setclicked({...clicked, isClicked:true});
-        setTimeout(()=>setclicked({...clicked, isClicked:false}), 350)
-    }}>{props.text}</button>
+     }}>{props.text}</button>
 }
 
 
